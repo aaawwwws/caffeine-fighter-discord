363ec7f2c78b9cc2d3e24dee21e6b8392782150f
ade ade <ochobase@outlook.jp>
2025-01-25 16:47:09 +0900

    test

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,48 @@
-const main = () => {};
+import * as dotenv from "dotenv";
+import {
+  GatewayIntentBits,
+  Client,
+  Partials,
+  Message,
+  SlashCommandBuilder,
+  REST,
+  Routes,
+} from "discord.js";
+const main = () => {
+  dotenv.config();
+  const TOKEN = process.env.TOKEN;
+  const SERVER_ID = process.env.SERVER_ID;
+  const CLIENT_ID = process.env.CLIENT_ID;
+
+  const test = new SlashCommandBuilder().setName("test").setDescription("test");
+
+  const commands = [test.toJSON()];
+
+  const client = new Client({
+    intents: [
+      GatewayIntentBits.DirectMessages,
+      GatewayIntentBits.Guilds,
+      GatewayIntentBits.GuildMembers,
+      GatewayIntentBits.GuildMessages,
+      GatewayIntentBits.MessageContent,
+    ],
+    partials: [Partials.Message, Partials.Channel],
+  });
+
+  const rest = new REST({ version: "10" }).setToken(TOKEN!);
+  rest.put(Routes.applicationGuildCommands(CLIENT_ID!, SERVER_ID!), {
+    body: commands,
+  });
+
+  client.once("ready", () => {
+    console.log("Ready!");
+    if (client.user) {
+      console.log(client.user.tag);
+    }
+  });
+
+  client.login(TOKEN!);
+};
 
 (async () => {
   main();
